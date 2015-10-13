ebfb5fe2fb1b608a77b3ca26e8e4c6b69cb1c59c
Ben <ben.c.hawker@gmail.com>
2015-10-13 12:43:44 +0100

    Initial large match feature test still to be satisfied

--- spec/features/matches_feature_spec.rb
+++ spec/features/matches_feature_spec.rb
@@ -9,17 +9,40 @@ feature 'matches' do
     end
   end
 
-  # context 'users have been added' do
-	 #  before do
-	 #    User.create(first_name: 'Ben', 
-	 #    						last_name: 'Hawker',
-	 #    						user_name: 'benhawker'
-	 #    						)
-	 #  end
+  context 'matches have been added' do
+	  before do
+	    Match.create(team_one: 'Team 1', 
+	    						team_two: 'Team 2',
+	    						team_one_score: '10',
+	    						team_two_score: '0'
+	    						)
+	  end
 
-	 #  scenario 'display users' do
-	 #    visit '/users'
-	 #    expect(page).to have_content('Ben')
-	 #    expect(page).not_to have_content('No users yet')
-	 #  end
+	  scenario 'display matches' do
+	    visit '/matches'
+	    expect(page).to have_content('Team 1 vs. Team 2')
+	    expect(page).not_to have_content('No matches yet')
+	  end
+	end
+
+	context 'creating matches' do
+		 	let!(:bob){ User.create(user_name: 'bob') }
+		 	let!(:ben){ User.create(user_name: 'ben') }
+		 	let!(:bill){ User.create(user_name: 'bill') }
+		 	let!(:joe){ User.create(user_name: 'joe') }
+
+		 	let!(:team_one){ Team.create(player_one: bob, player_two: ben) }
+	  	let!(:team_two){ Team.create(player_one: bill, player_two: joe) }
+
+	  scenario 'prompts user to fill out a form, then displays the new match up' do
+	    visit '/match'
+	    click_link 'Add new match'
+	    fill_in 'match[team_one]', with: 'bob & ben'
+	   	fill_in 'match[team_two]', with: 'bill & joe'
+	    click_button 'Create match'
+	    expect(page).to have_content ''
+	    expect(current_path).to eq '/users'
+	  end
+	end
+
 	end
