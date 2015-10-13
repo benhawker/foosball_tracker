ea4489602db955a95d4ef42a0d419a43a165ad20
Ben <ben.c.hawker@gmail.com>
2015-10-13 11:03:22 +0100

    Team create action completed - feature test passing

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,4 +6,20 @@ class TeamsController < ApplicationController
 	def new
 	  @team = Team.new
 	end
+
+	def create
+    @team = Team.new(user_params)
+    if @team.save
+    	flash[:notice] = 'Team created successfully'
+    	redirect_to teams_path
+    else
+    	render 'new'
+    end
+  end
+
+ 	def user_params
+    params.require(:team).permit(:player_one, :player_two, 
+    														:wins, :win_percentage)
+  end
+  
 end
